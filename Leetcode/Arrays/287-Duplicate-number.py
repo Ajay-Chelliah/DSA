0befd36c91b666f7f76a1b40dfb6875a292afdcb
Ajay-Chelliah <ajay8425p@gmail.com>
2025-06-23 23:58:14 +0530

    Day 5

---
+++ Leetcode/Arrays/287-Duplicate-number.py
@@ -0,0 +1,13 @@
+def findDuplicate(nums: list[int]) -> int:
+    slow = nums[0]
+    fast = nums[0]
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
+    fast = nums[0]
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+    return slow
