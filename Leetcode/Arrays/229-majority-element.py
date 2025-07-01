679ed779c67c51f3da44027c219c72c3c7c51da2
Ajay-Chelliah <ajay8425p@gmail.com>
2025-07-01 20:16:23 +0530

    Day 1

--- Leetcode/Arrays/229-majority-element.py
+++ Leetcode/Arrays/229-majority-element.py
@@ -1,13 +1,33 @@
 def majorityElement(nums):
-    list1 = []
-    dict1 = {}
     n = len(nums)
-    for i in nums:
-        if i in dict1:
-            dict1[i] += 1
-            continue
-        dict1[i] = 1
-    for i, j in dict1.items():
-        if j > (n // 3):
-            list1.append(i)
-    return list1
+    cnt1, cnt2 = 0, 0  # counts
+    el1, el2 = float("-inf"), float("-inf")
+    for i in range(n):
+        if cnt1 == 0 and el2 != nums[i]:
+            cnt1 = 1
+            el1 = nums[i]
+        elif cnt2 == 0 and el1 != nums[i]:
+            cnt2 = 1
+            el2 = nums[i]
+        elif nums[i] == el1:
+            cnt1 += 1
+        elif nums[i] == el2:
+            cnt2 += 1
+        else:
+            cnt1 -= 1
+            cnt2 -= 1
+
+    ls = []
+    cnt1, cnt2 = 0, 0
+    for i in range(n):
+        if nums[i] == el1:
+            cnt1 += 1
+        if nums[i] == el2:
+            cnt2 += 1
+
+    mini = int(n / 3) + 1
+    if cnt1 >= mini:
+        ls.append(el1)
+    if cnt2 >= mini:
+        ls.append(el2)
+    return ls
